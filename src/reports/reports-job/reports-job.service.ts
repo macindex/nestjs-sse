9d33407d7bdc2c74f1cc540eaa59f93940520878
macindex <machollanda@gmail.com>
2023-06-20 14:17:16 -0300

    reports job service start

--- src/reports/reports-job/reports-job.service.ts
+++ src/reports/reports-job/reports-job.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Processor } from '@nestjs/bull';
+import { ReportsService } from '../reports/reports.service';
 
-@Injectable()
-export class ReportsJobService {}
+@Processor('reports')
+export class ReportsJobService {
+    constructor(private reportsService: ReportsService){}
+    produce(job: Job<{ reportId: number }>){
+        this.reportsService.produce(job.data.reportId);
+        return{};
+    }
+}
