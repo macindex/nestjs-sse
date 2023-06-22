0036084a9ab3e54e92d9218aaff448896e36f705
macindex <machollanda@gmail.com>
2023-06-22 10:05:14 -0300

    route controller

--- src/reports/reports/reports.controller.ts
+++ src/reports/reports/reports.controller.ts
@@ -1,33 +1,49 @@
-import { Controller, Get, MessageEvent, Param, ParseIntPipe, Post, Sse } from '@nestjs/common';
+import { Controller, Get, MessageEvent, Param, ParseIntPipe, Post, Render, Res, Sse } from '@nestjs/common';
 import { ReportsService } from './reports.service';
-import { defer, map, Observable, repeat } from 'rxjs'
+import { defer, map, Observable, repeat, tap } from 'rxjs'
+import { Status } from '@prisma/client';
+import { Response } from 'express';
 
 @Controller('reports')
 export class ReportsController {
-    constructor(private reportsService: ReportsService){
-
+    constructor(private reportsService: ReportsService) {}
+    @Get('view')
+    @Render('reports')
+    async view() {
+        const reports = await this.reportsService.all();
+        return { reports };
     }
     @Get()
-    all(){
+    all() {
         return this.reportsService.all();
     }
     @Get(':id')
-    findOne(@Param('id', new ParseIntPipe()) id: number){
+    findOne(@Param('id', new ParseIntPipe()) id: number) {
         return this.reportsService.findOne(id);
     }
     @Post()
-    request(){
+    request() {
         return this.reportsService.request();
     }
     @Sse(':id/events')
-    events(@Param('id', new ParseIntPipe()) id: number, ): Observable<MessageEvent>{
-        return defer(() => this.reportsService.findOne(id)).pipe(repeat({
-            delay: 1000,
-        }),
-        map((report) => ({
-            type: 'message',
-            data: report,
-        })),
+    events(@Param('id', new ParseIntPipe()) id: number,
+        @Res() response: Response
+    ): Observable<MessageEvent> {
+        return defer(() => this.reportsService.findOne(id)).pipe(
+            repeat({
+                delay: 1000,
+            }),
+            tap((report) => {
+                if (report.status === Status.DONE || report.status === Status.ERROR) {
+                    setTimeout(() => {
+                        response.end();
+                    }, 1000);
+                }
+            }),
+            map((report) => ({
+                type: 'message',
+                data: report,
+            })),
         );
     }
 }
